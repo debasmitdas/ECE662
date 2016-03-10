98876203cffc7e9e90ebffa0a65586a0db654c07
debasmitdas <debasmit.das@gmail.com>
2016-03-10 01:29:30 -0500

    CPU Time Code Added

--- MiniProject2/mle_normal.m
+++ MiniProject2/mle_normal.m
@@ -1,6 +1,6 @@
 function [mu, sigma]=mle_normal(Data) %Each Row is one example 
 % So if we have N trainign examples there are N rows
-N=size(Data,1)
+N=size(Data,1);
 mu=mean(Data);
 sigma=(cov(Data)*(N-1))/(N);
 %sigma=cov(Data,1);
